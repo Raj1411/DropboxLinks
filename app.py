ffa1573d686d2d5e08dff15a3de4ddbb094bbd96
Raj1411 <56500451+Raj1411@users.noreply.github.com>
2021-07-26 00:22:13 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ option.add_argument('--no-sandbox')
 option.add_argument('--disable-dev-shm-usage')
 # driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), options=option)
 
-driver=webdriver.Chrome(executable_path="/home/appuser/.conda/bin/chrome-driver",options=option)
+driver=webdriver.Chrome(executable_path="/home/appuser/.conda/bin/chromedriver",options=option)
 headers1 = {
     'Accept-Encoding': 'gzip, deflate, sdch',
     'Accept-Language': 'en-US,en;q=0.8',
